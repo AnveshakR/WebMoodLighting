a15861254e4534cf0cce656b9a833d945d3eb023
AnveshakR <rathoreanveshak@gmail.com>
2022-01-02 02:39:24 +0530

    new webpage, hardcoded breathe

--- led_web.ino
+++ led_web.ino
@@ -5,61 +5,79 @@
 #include <FastLED.h>
 #define DATA D4
 #define NUM_LEDS 20 
+void color_set(String,String,AsyncWebServerRequest);
 
 AsyncWebServer server(80);
 
+String hex_val,picker_val,display_radio_val;
+int breath_val=1000,r_val,g_val,b_val;
 const char* ssid = "sad wifi";
 const char* password = "sadpassword";
-String r_val,g_val,b_val,hex_val,picker_val;
-const char* red_param = "red_input";
-const char* green_param = "green_input";
-const char* blue_param = "blue_input";
 const char* hex_param = "hex_input";
 const char* picker_param = "picker_input";
+const char* color_radio = "color_input";
+const char* display_radio = "display_input";
+const char* breath_param = "breath_input";
 
 CRGB leds[NUM_LEDS];
 
 const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html><head>
-  <title>LED Control</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <style>
-    html {font-family: Arial; display: inline-block; text-align: center;}
-    h2 {font-size: 3.0rem; color: #0000FF;}
-  </style>
-  </head><body>
-  <h2>LED Control</h2> 
-  <p>Enter RGB values(0-255)</p>
-  <form action="/">
-    <label for="r">Red: </label> 
-    <input type="number" name="red_input" id="red" step="1" min="0" max="255">
-    
-    <label for="g">Green: </label> 
-    <input type="number" name="green_input" id="green" step="1" min="0" max="255">
-    
-    <label for="b">Blue: </label> 
-    <input type="number" name="blue_input" id="blue" step="1" min="0" max="255">
-    
-    <button><input type="submit" value="Submit"></button>
-  </form><br>
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>LED Control</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+      html {
+        font-family: Sans-Serif;
+        display: block;
+        background-color: #222831;
+        color: #EEEEEE;
+        text-align: justify
+      }
 
-  <p>OR</p>
-  
-  <form action="/">
-    <label for="hex">Enter hex value: #</label>
-    <input type="text" name="hex_input" id="hex">
-    <button><input type="submit" value="Submit"></button>
-  </form><br>  
-  
-  <p>OR</p>
-  
-  <form action="/">
-    <label for="picker">Select a color:</label>
-    <input type="color" name="picker_input" id="picker">
-    <button><input type="submit" value="Submit"></button>
-  </form><br>
-  
-</body></html>)rawliteral";
+      p {
+        color: #EEEEEE;
+      }
+
+      h2 {
+        font-size: 3.0rem;
+        color: #7BC74D
+      }
+
+      input {
+        background-color: #393E46;
+        color: #EEEEEE
+      }
+
+      form {
+        padding: 15px
+      }
+
+      ;
+    </style>
+  </head>
+  <body>
+    <h2>LED Control</h2>
+    <form action="/">
+      <p>Select color:</p>
+      <input type="radio" id="hex" name="color_input" value="hex">Hex value: # <input type="text" name="hex_input" id="hex" />
+      <br>
+      <br>
+      <input type="radio" id="picker" name="color_input" value="picker">Pick a color: <input type="color" name="picker_input" id="picker" />
+      <br>
+      <br>
+      <p>Select display type:</p>
+      <input type="radio" id="solid" name="display_input" value="solid">Solid Color <br>
+      <br>
+      <input type="radio" id="breathing" name="display_input" value="breathing">Breathing: <input type="number" name="breath_input" id="breath" />ms 
+      <br>
+      <br>
+      <br>
+      <button><input type="submit" value="Submit"></button>
+    </form>
+  </body>
+</html>)rawliteral";
 
 void notFound(AsyncWebServerRequest *request) {
   request->send(404, "text/plain", "Not found");
@@ -81,43 +99,87 @@ void setup() {
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send_P(200, "text/html", index_html);
     String input_parameter;
-    if (request->hasParam(red_param) and request->hasParam(green_param) and request->hasParam(blue_param)) {
-      r_val = request->getParam(red_param)->value();
-      g_val = request->getParam(green_param)->value();
-      b_val = request->getParam(blue_param)->value();
+    if (request->hasParam("color_input") and request->hasParam("display_input")) {
+      String radio1 = request->getParam(color_radio)->value();
+      display_radio_val = request->getParam(display_radio)->value();
+      color_set(radio1, request);
     }
 
-    else if (request->hasParam(hex_param)){
-      hex_val = request->getParam(hex_param)->value();
-      long number = strtol(&hex_val[0], NULL, 16);     
-      r_val = String(number >> 16);
-      g_val = String(number >> 8 & 0xFF);
-      b_val = String(number & 0xFF);
-    }
-    
-    else if (request->hasParam(picker_param)){
-      picker_val = request->getParam(picker_param)->value();
-      long number = strtol( &picker_val[1], NULL, 16);     
-      r_val = String(number >> 16);
-      g_val = String(number >> 8 & 0xFF);
-      b_val = String(number & 0xFF);
-    }
     else {}
   });
   server.onNotFound(notFound);
   server.begin();
 }
 
+void color_set(String color_radio_val, AsyncWebServerRequest *request)
+{
+  
+  if(color_radio_val=="hex")
+  {
+    hex_val = request->getParam(hex_param)->value();
+    long number = strtol(&hex_val[0], NULL, 16);     
+    r_val = String(number >> 16).toInt();
+    g_val = String(number >> 8 & 0xFF).toInt();
+    b_val = String(number & 0xFF).toInt();
+  }
+  else if(color_radio_val=="picker")
+  {
+    picker_val = request->getParam(picker_param)->value();
+    long number = strtol( &picker_val[1], NULL, 16);     
+    r_val = String(number >> 16).toInt();
+    g_val = String(number >> 8 & 0xFF).toInt();
+    b_val = String(number & 0xFF).toInt();
+  }
+}
+
 void loop()
 { 
-  byte r,g,b;
-  for (int cur = 0; cur < NUM_LEDS; cur++) 
+  if (display_radio_val=="solid")
   {
-    r = r_val.toInt();
-    g = g_val.toInt();
-    b = b_val.toInt();
-    leds[cur] = CRGB(r,g,b);
-    FastLED.show();
-    FastLED.delay(10);
+    for (int cur = 0; cur < NUM_LEDS; cur++) 
+    {
+      leds[cur] = CRGB(r_val,g_val,b_val);
+      FastLED.show();
+      FastLED.delay(10);
+    }
+  }
+  else if (display_radio_val=="breathing")
+  {
+//    r = r_val.toInt();
+//    g = g_val.toInt();
+//    b = b_val.toInt();
+//    for (int c=0; c<NUM_LEDS; c++)
+//      {
+//        leds[c] = CRGB(r_val,g_val,b_val);
+//        FastLED.show();
+//        FastLED.delay(5);
+//      }
+    Serial.println("dimming");
+    for (int i=255; i>0; i--)
+    {
+      Serial.println(i);
+      for (int c=0; c<NUM_LEDS; c++)
+      {
+        leds[c] = CRGB(r_val,g_val,b_val);
+        FastLED.setBrightness(i);     
+        FastLED.show();
+        //FastLED.delay(5);
+      }
+      FastLED.delay(round(breath_val/510));
+    }
+    Serial.println("lighting");
+    for (int i=0; i<255; i++)
+    {
+     Serial.println(i);
+     for (int c=0; c<NUM_LEDS; c++)
+      {
+        
+        leds[c] = CRGB(r_val,g_val,b_val);
+        FastLED.setBrightness(i);     
+        FastLED.show();
+        //FastLED.delay(5);
+      }
+      FastLED.delay(round(breath_val/510));
+    } 
   }
 }
